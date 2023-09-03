280f7773f2b52ef9442ec6e6a1c3f54d229d4e87
Virendara <virendara1224@gmail.com>
2023-09-04 01:28:48 +0530

    js and css file included

---
+++ tut3.js
@@ -0,0 +1,18 @@
+(function (){
+const buttons=document.querySelector('.button')
+let count=0;
+
+buttons.addEventListener('click' , function(e){
+
+
+e.preventDefault(){
+    
+}
+
+
+})
+
+
+
+
+})
