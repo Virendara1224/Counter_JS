7400e70bc6a822c4e7008164a59192ba33203965
Virendara <virendara1224@gmail.com>
2023-09-04 02:05:37 +0530

    final commit

--- tut3.js
+++ tut3.js
@@ -1,18 +1,31 @@
 (function (){
-const buttons=document.querySelector('.button')
+const buttons=document.querySelectorAll('.button')
 let count=0;
+const counter=document.querySelector('#counter');
+counter.textContent=count;
+buttons.forEach(function(button){
+    button.addEventListener('click' , function(){
 
-buttons.addEventListener('click' , function(e){
+        if(button.classList.contains('Mbtn')){
+            count--;
+        }
+        else if(button.classList.contains('Pbtn')){
+            count++;
+        }
+        counter.textContent=count;
 
+        if(count<0){
+         counter.style.color='red'
+        }
+        else if(count>0){
+            counter.style.color='green';
+         }
+         else{
+            counter.style.color='#333333';
+         }
+        
+        
+        })
+});
 
-e.preventDefault(){
-    
-}
-
-
-})
-
-
-
-
-})
+})()
